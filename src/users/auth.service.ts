35acc6a44714848b08ba762a23d8a49fe161cd77
denis_levchenko <tranzi90@gmail.com>
2024-01-14 21:35:19 +0300

    + Sign In function

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
-
 import { promisify } from 'util';
 
 const scrypt = promisify(_scrypt);
@@ -24,6 +27,24 @@ export class AuthService {
     const result = salt + '.' + hash.toString('hex');
 
     const user = await this.usersService.create(email, result);
+
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('incorrect password');
+    }
 
     return user;
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,9 +24,15 @@ export class UsersController {
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     return this.authService.signup(body.email, body.password);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
   }
 
   @Get('/:id')
